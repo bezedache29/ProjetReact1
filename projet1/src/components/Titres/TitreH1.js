218fa863f6e525b1c802752040454e80ddbf2022
unknown <bezedache29@gmail.com>
2020-09-14 18:18:37 +0200

    Composant titre

--- projet1/src/components/Titres/TitreH1.js
+++ projet1/src/components/Titres/TitreH1.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import css from "./TitreH1.module.css"
 
 const titreH1 = (props) => {
+    const monCss = `border border-dark p2 mt-2 text-white text-center bg-primary rounded ${css.policeTitre}`
+    
     return (
         <>
-            <h1 className = "border border-dark p2 mt-2 text-white text-center bg-primary rounded">{props.children}</h1>
+            <h1 className = {monCss}>{props.children}</h1>
         </>
     )
 }
