e197e4dc3fd25d9393bfaf603806c4cd31037338
unknown <bezedache29@gmail.com>
2020-09-21 15:47:40 +0200

    Mise en ligne du projet1

--- projet1/src/App.js
+++ projet1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import TitreH1 from './components/Titres/TitreH1'
 import Bouton from './components/Boutons/Bouton'
 import Livres from './containers/Livres/Livres'
+import {BrowserRouter} from 'react-router-dom'
 
 class App extends Component {
     state = {
@@ -22,11 +23,13 @@ class App extends Component {
     render() {
         return (
             <Fragment>
-                <div className="container">
-                    <TitreH1>Page listant les livres</TitreH1>
-                    <Livres ajout={this.state.ajout} fermerAjoutLivre={() => {this.setState({ajout: true})}} />
-                    <Bouton btn = "btn-success" clic = {() => this.handleAjout()} width = "w-100">{(this.state.ajout) ? "Ajouter" : "Fermer Ajout"}</Bouton>
-                </div>
+                <BrowserRouter basename="/js/react/projet1">
+                    <div className="container">
+                        <TitreH1>Page listant les livres</TitreH1>
+                        <Livres ajout={this.state.ajout} fermerAjoutLivre={() => {this.setState({ajout: true})}} />
+                        <Bouton btn = "btn-success" clic = {() => this.handleAjout()} width = "w-100">{(this.state.ajout) ? "Ajouter" : "Fermer Ajout"}</Bouton>
+                    </div>
+                </BrowserRouter>
             </Fragment>
         )
     }
